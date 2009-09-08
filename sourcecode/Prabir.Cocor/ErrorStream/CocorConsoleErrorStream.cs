3dad54760ae7bbff82ad37f85591381542476684
Prabir Shrestha <mail@prabir.me>
2009-09-08 20:38:26 +0700

    + CocorConsoleErrorStream

---
+++ sourcecode/Prabir.Cocor/ErrorStream/CocorConsoleErrorStream.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace Prabir.Cocor
+{
+    public class CocorConsoleErrorStream : TextWriter
+    {
+        // Just implement only these; coz Coco uses only them.
+
+        public override void WriteLine(string format, object arg0, object arg1, object arg2)
+        {
+            Console.WriteLine("X"+string.Format(format, arg0, arg1, arg2));
+        }
+
+        public override void WriteLine(string value)
+        {
+            Console.WriteLine(value);
+        }
+
+        public override System.Text.Encoding Encoding
+        {
+            get { return System.Text.Encoding.ASCII; }
+        }
+    }
+}
--- sourcecode/cocor/Program.cs
+++ sourcecode/cocor/Program.cs
@@ -42,79 +42,93 @@ Coco/R itself) does not fall under the GNU General Public License.
 
 using System;
 using System.IO;
+using Prabir.Cocor;
 
-namespace at.jku.ssw.Coco {
+namespace at.jku.ssw.Coco
+{
 
-public class Coco {
-		
-	public static int Main (string[] arg) {
-		Console.WriteLine("Coco/R (Jun 22, 2009)");
-		string srcName = null, nsName = null, frameDir = null, ddtString = null,
-		traceFileName = null, outDir = null;
-		int retVal = 1;
-		for (int i = 0; i < arg.Length; i++) {
-			if (arg[i] == "-namespace" && i < arg.Length - 1) nsName = arg[++i];
-			else if (arg[i] == "-frames" && i < arg.Length - 1) frameDir = arg[++i]; 
-			else if (arg[i] == "-trace" && i < arg.Length - 1) ddtString = arg[++i]; 
-			else if (arg[i] == "-o" && i < arg.Length - 1) outDir = arg[++i]; 
-			else srcName = arg[i];
-		}
-		if (arg.Length > 0 && srcName != null) {
-			try {
-				string srcDir = Path.GetDirectoryName(srcName);
-				
-				Scanner scanner = new Scanner(srcName);
-				Parser parser = new Parser(scanner);
+    public class Coco
+    {
 
-				traceFileName = Path.Combine(srcDir, "trace.txt");
-				parser.trace = new StreamWriter(new FileStream(traceFileName, FileMode.Create));
-				parser.tab = new Tab(parser);
-				parser.dfa = new DFA(parser);
-				parser.pgen = new ParserGen(parser);
+        public static int Main(string[] arg)
+        {
+            Console.WriteLine("Coco/R (Jun 22, 2009) - Modified by Prabir Shrestha");
+            Console.WriteLine("For more info please visit http://projects.prabir.me/compiler");
+            string srcName = null, nsName = null, frameDir = null, ddtString = null,
+            traceFileName = null, outDir = null;
+            int retVal = 1;
+            for (int i = 0; i < arg.Length; i++)
+            {
+                if (arg[i] == "-namespace" && i < arg.Length - 1) nsName = arg[++i];
+                else if (arg[i] == "-frames" && i < arg.Length - 1) frameDir = arg[++i];
+                else if (arg[i] == "-trace" && i < arg.Length - 1) ddtString = arg[++i];
+                else if (arg[i] == "-o" && i < arg.Length - 1) outDir = arg[++i];
+                else srcName = arg[i];
+            }
+            if (arg.Length > 0 && srcName != null)
+            {
+                try
+                {
+                    string srcDir = Path.GetDirectoryName(srcName);
 
-				parser.tab.srcName = srcName;
-				parser.tab.srcDir = srcDir;
-				parser.tab.nsName = nsName;
-				parser.tab.frameDir = frameDir;
-				parser.tab.outDir = (outDir != null) ? outDir : srcDir;
-				if (ddtString != null) parser.tab.SetDDT(ddtString);
+                    Scanner scanner = new Scanner(srcName);
+                    Parser parser = new Parser(scanner);
 
-				parser.Parse();
+                    traceFileName = Path.Combine(srcDir, "trace.txt");
+                    parser.trace = new StreamWriter(new FileStream(traceFileName, FileMode.Create));
+                    parser.tab = new Tab(parser);
+                    parser.dfa = new DFA(parser);
+                    parser.pgen = new ParserGen(parser);
 
-				parser.trace.Close();
-				FileInfo f = new FileInfo(traceFileName);
-				if (f.Length == 0) f.Delete();
-				else Console.WriteLine("trace output is in " + traceFileName);
-				Console.WriteLine("{0} errors detected", parser.errors.count);
-				if (parser.errors.count == 0) { retVal = 0; }
-			} catch (IOException) {
-				Console.WriteLine("-- could not open " + traceFileName);
-			} catch (FatalError e) {
-				Console.WriteLine("-- " + e.Message);
-			}
-		} else {
-			Console.WriteLine("Usage: Coco Grammar.ATG {{Option}}{0}" +
-			                  "Options:{0}" +
-			                  "  -namespace <namespaceName>{0}" +
-			                  "  -frames    <frameFilesDirectory>{0}" +
-			                  "  -trace     <traceString>{0}" +
-			                  "  -o         <outputDirectory>{0}" +
-			                  "Valid characters in the trace string:{0}" +
-			                  "  A  trace automaton{0}" +
-			                  "  F  list first/follow sets{0}" +
-			                  "  G  print syntax graph{0}" +
-			                  "  I  trace computation of first sets{0}" +
-			                  "  J  list ANY and SYNC sets{0}" +
-			                  "  P  print statistics{0}" +
-			                  "  S  list symbol table{0}" +
-			                  "  X  list cross reference table{0}" +
-			                  "Scanner.frame and Parser.frame files needed in ATG directory{0}" +
-                        "or in a directory specified in the -frames option.",
-			                  Environment.NewLine);
-		}
-		return retVal;
-	}
-		
-} // end Coco
+                    parser.tab.srcName = srcName;
+                    parser.tab.srcDir = srcDir;
+                    parser.tab.nsName = nsName;
+                    parser.tab.frameDir = frameDir;
+                    parser.tab.outDir = (outDir != null) ? outDir : srcDir;
+                    if (ddtString != null) parser.tab.SetDDT(ddtString);
+                    //parser.errors.errorStream = new CocorConsoleErrorStream();
+                    parser.Parse();
+
+                    parser.trace.Close();
+                    FileInfo f = new FileInfo(traceFileName);
+                    if (f.Length == 0) f.Delete();
+                    else Console.WriteLine("trace output is in " + traceFileName);
+                    Console.WriteLine("{0} errors detected", parser.errors.count);
+                    if (parser.errors.count == 0) { retVal = 0; }
+                }
+                catch (IOException)
+                {
+                    Console.WriteLine("-- could not open " + traceFileName);
+                }
+                catch (FatalError e)
+                {
+                    Console.WriteLine("-- " + e.Message);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Usage: Coco Grammar.ATG {{Option}}{0}" +
+                                  "Options:{0}" +
+                                  "  -namespace <namespaceName>{0}" +
+                                  "  -frames    <frameFilesDirectory>{0}" +
+                                  "  -trace     <traceString>{0}" +
+                                  "  -o         <outputDirectory>{0}" +
+                                  "Valid characters in the trace string:{0}" +
+                                  "  A  trace automaton{0}" +
+                                  "  F  list first/follow sets{0}" +
+                                  "  G  print syntax graph{0}" +
+                                  "  I  trace computation of first sets{0}" +
+                                  "  J  list ANY and SYNC sets{0}" +
+                                  "  P  print statistics{0}" +
+                                  "  S  list symbol table{0}" +
+                                  "  X  list cross reference table{0}" +
+                                  "Scanner.frame and Parser.frame files needed in ATG directory{0}" +
+                            "or in a directory specified in the -frames option.",
+                                  Environment.NewLine);
+            }
+            return retVal;
+        }
+
+    } // end Coco
 
 } // end namespace
