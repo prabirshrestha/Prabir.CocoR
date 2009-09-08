5b04ad778bc7c94235b37b1927b9b701c778cb45
Prabir Shrestha <mail@prabir.me>
2009-09-08 16:16:39 +0700

    Added Empty cocor C# console application

---
+++ sourcecode/cocor/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cocor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
