dc5f775ba98d1a6419e21ede255793b706a084f7
Prabir Shrestha <mail@prabir.me>
2009-09-09 00:13:50 +0700

    + added strong key + renamed CocorConsoleErrorStream to CocorConsoleTextWriter

--- sourcecode/Prabir.Cocor/TextWriters/CocorConsoleTextWriter.cs
+++ sourcecode/Prabir.Cocor/TextWriters/CocorConsoleTextWriter.cs
@@ -3,13 +3,13 @@ using System.IO;
 
 namespace Prabir.Cocor
 {
-    public class CocorConsoleErrorStream : TextWriter
+    public class CocorConsoleTextWriter : TextWriter
     {
         // Just implement only these; coz Coco uses only them.
 
         public override void WriteLine(string format, object arg0, object arg1, object arg2)
         {
-            Console.WriteLine("X"+string.Format(format, arg0, arg1, arg2));
+            Console.WriteLine(string.Format(format, arg0, arg1, arg2));
         }
 
         public override void WriteLine(string value)
