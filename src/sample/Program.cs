283e1e786fda67c8beb9fea3c7af947acf18211b
Prabir Shrestha <mail@prabir.me>
2010-08-01 15:56:22 +0700

    + cocor t4 templates

---
+++ src/sample/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
